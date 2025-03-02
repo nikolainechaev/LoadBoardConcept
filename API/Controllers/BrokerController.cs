cc2707081daf9fe4d26e3169527dcc1af5c86114
nikolainechaev <40309434+nikolainechaev@users.noreply.github.com>
2025-03-02 09:25:53 -0500

    added 2 more endpoints for broker role

--- API/Controllers/BrokerController.cs
+++ API/Controllers/BrokerController.cs
@@ -19,4 +19,31 @@ public class BrokerController : ControllerBase
 		var docRef = await collection.AddAsync(broker);
 		return Ok(new { Id = docRef.Id });
 	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetBrokers()
+	{
+		var collection = _firestore.Collection("brokers");
+		var snapshot = await collection.GetSnapshotAsync();
+		var brokers = new List<Broker>();
+
+		foreach (var doc in snapshot.Documents)
+		{
+			var broker = doc.ConvertTo<Broker>();
+			broker.Id = doc.Id;
+			brokers.Add(broker);
+		}
+		return Ok(brokers);
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetBrokerById(string id)
+	{
+		var doc = await _firestore.Collection("brokers").Document(id).GetSnapshotAsync();
+		if (!doc.Exists) return NotFound("Carrier not found.");
+
+		var broker = doc.ConvertTo<Broker>();
+		broker.Id = doc.Id;
+		return Ok(broker);
+	}
 }
